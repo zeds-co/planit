46870f4a644c302167ac2e052766dcf56b281723
nouvelle <nouvellenuts@gmail.com>
2019-11-19 17:00:25 +0900

    create: dashboard page ui

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <dashboard location="Shanghai" term="3" date="12/27/2019 - 12/29/2019" />
+  </div>
+</template>
+
+<script>
+import Dashboard from "./components/Dashboard";
+
+export default {
+  name: "app",
+  components: {
+    dashboard: Dashboard
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="dashboard">
+    <div class="header">
+      <div class="locationWrap">
+        <div class="location">{{ location }}</div>
+        <div class="term">{{ term }} DAY TRIP ( {{ date }} )</div>
+      </div>
+      <div class="exchangeWrap">
+        <div class="exchange">1 CNY = 15.4621 JPY</div>
+        <div class="exchange">100 JPY = 6.46743 CNY</div>
+      </div>
+    </div>
+    <div class="contents">
+      <div class="dayWrap">
+        <div class="dayHeader">
+          <div class="day">DAY1 (12/27/2019)</div>
+          <div class="weather">SUNNY</div>
+        </div>
+        <div class="itineraryWrap">
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+        </div>
+      </div>
+      <div class="dayWrap">
+        <div class="dayHeader">
+          <div class="day">DAY2 (12/28/2019)</div>
+          <div class="weather">SNOW</div>
+        </div>
+        <div class="itineraryWrap">
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+        </div>
+      </div>
+      <div class="dayWrap">
+        <div class="dayHeader">
+          <div class="day">DAY3 (12/29/2019)</div>
+          <div class="weather">SNOW</div>
+        </div>
+        <div class="itineraryWrap">
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+          <div class="itinerary">
+            <button class="addBtn" v-on:click="addPlan">+</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  props: {
+    location: String,
+    term: String,
+    date: String
+  },
+  methods: {
+    addPlan() {
+      // store.commit("Click Add Button");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.dashboard {
+  padding: 10px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+.location {
+  font-size: 42px;
+}
+.contents {
+  display: flex;
+  overflow-x: scroll;
+  background: #eee;
+  height: 80%;
+  padding: 10px;
+  margin-top: 50px;
+}
+.dayHeader {
+  display: flex;
+  justify-content: space-between;
+  margin: 15px;
+  font-size: 20px;
+}
+.dayWrap {
+  height: 500px;
+  flex: 0 0 350px;
+}
+.itineraryWrap {
+  border: 1px #999 solid;
+  margin: 15px;
+}
+.itinerary {
+  width: 90%;
+  height: 100px;
+  border: 1px #ccc solid;
+  margin: 15px auto;
+}
+.addBtn {
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  border: none;
+}
+</style>
