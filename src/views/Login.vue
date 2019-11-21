a55fd1344866e369e1e8103edf06d40513831697
steffieharrner <steffie.harner@gmail.com>
2019-11-21 11:56:46 +0900

    o auth attempted

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,16 +7,83 @@
     <br />
     <button>connection</button>
     <p>don't have an account? create one</p>
+
+    <button>
+      <h1>Test</h1>
+      <button @click="handleClickGetAuth" :disabled="!isInit">
+        get auth code
+      </button>
+      <button @click="handleClickSignIn" v-if="!isSignIn" :disabled="!isInit">
+        signIn
+      </button>
+      <button @click="handleClickSignOut" v-if="isSignIn" :disabled="!isInit">
+        signOout
+      </button>
+    </button>
   </div>
 </template>
 
 <script>
+// import firebase from "firebase";
+
 export default {
   name: "Login",
   data() {
-    return {};
+    return {
+      isInit: false,
+      isSignIn: false
+    };
   },
-  methods: {}
+  methods: {
+    handleClickGetAuth() {
+      this.$gAuth
+        .getAuthCode()
+        .then(authCode => {
+          // On success
+          return this.$http.post("http://your-backend-server.com/auth/google", {
+            code: authCode,
+            redirect_uri: "postmessage"
+          });
+        })
+        .then(response => {
+          // And then
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    handleClickSignIn() {
+      this.$gAuth
+        .signIn()
+        .then(user => {
+          // On success do something, refer to https://developers.google.com/api-client-library/javascript/reference/referencedocs#googleusergetid
+          console.log("user", GoogleUser);
+          this.isSignIn = this.$gAuth.isAuthorized;
+        })
+        .catch(error => {
+          // On fail do something
+        });
+    },
+    handleClickSignOut() {
+      this.$gAuth
+        .signOut()
+        .then(() => {
+          // On success do something
+          this.isSignIn = this.$gAuth.isAuthorized;
+        })
+        .catch(error => {
+          // On fail do something
+        });
+    }
+  },
+  mounted() {
+    let that = this;
+    let checkGauthLoad = setInterval(function() {
+      that.isInit = that.$gAuth.isInit;
+      that.isSignIn = that.$gAuth.isAuthorized;
+      if (that.isInit) clearInterval(checkGauthLoad);
+    }, 1000);
+  }
 };
 </script>
 
